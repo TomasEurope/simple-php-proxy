3800dedf517ee59a36b4c1483501ab23b2673ab1
Tomas Studnicny <tomass@xcroco.com>
2024-12-10 09:07:14 +0100

    CC Script

--- cc.sh
+++ cc.sh
@@ -1,9 +1,13 @@
 clear
 
+echo -e "\n\n# psalm\n\n"
 vendor/bin/psalm --show-info=true
 
+echo -e "\n\n# phpstan\n\n"
 vendor/bin/phpstan analyze
 
+echo -e "\n\n# lint\n\n"
 vendor/bin/parallel-lint --exclude vendor .
 
+echo -e "\n\n# phpcs\n\n"
 vendor/bin/phpcs --standard=PSR12 --extensions=php --tab-width=4 -sp src public
