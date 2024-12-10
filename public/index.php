934044908f574996791d567df00e9fc32d0ceb10
Tomas Studnicny <tomass@xcroco.com>
2024-12-10 09:20:00 +0100

    psalm-suppress MissingFile

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ use function error_reporting;
 // Autoload dependencies (Slim, Guzzle, and other packages).
 // TODO Relative path doesn't work...?
 /**
- * @psalm-suppress FileNotFounds
+ * @psalm-suppress MissingFile
  */
 require __DIR__ . '/../vendor/autoload.php';
 
